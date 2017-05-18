6d356e2b21660526c33af7cc60997b3f20e64764
Unknown <thomas.esteoulle@hotmail.fr>
2017-05-18 17:58:12 +0200

    Routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,30 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
 
   resources :users
   get 'users/new'
-  get  '/signup',  to: 'users#new'
+  get  '/signin' => 'users#new'
+
+  get '/actu/alsace' => 'other_pages#articles'
+  get '/actu/bretagne' => 'other_pages#articles'
+  get '/actu/67' => 'other_pages#articles'
+  get '/actu/68' => 'other_pages#articles'
+  get '/actu/22' => 'other_pages#articles'
+  get '/actu/29' => 'other_pages#articles'
+  get '/actu/35' => 'other_pages#articles'
+  get '/actu/56' => 'other_pages#articles'
+
+  get '/bonsplans/alsace' => 'other_pages#deals'
+  get '/bonsplans/bretagne' => 'other_pages#deals'
+
+  get '/lieux/alsace' => 'other_pages#places'
+  get '/lieux/bretagne' => 'other_pages#places'
+
+  get '/survie' => 'other_pages#survival'
+
+  #get 'index' => 'other_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
