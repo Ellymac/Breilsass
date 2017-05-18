ce203c4a32ef6d794745852d3ce2ecd2a1a91851
Ellymac <cschnell67@gmail.com>
2017-05-18 18:20:59 +0200

    Connexion possible + plus de problème d'affichage connexion/inscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'static_pages/help'
@@ -6,5 +8,9 @@ Rails.application.routes.draw do
   resources :users
   get 'users/new'
   get  '/signup',  to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
