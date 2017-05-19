bc02fbe7b766efaa2523487738d4b8840360489d
ThomasE <thomas.esteoulle@hotmail.fr>
2017-05-19 12:44:15 +0200

    dynamic titles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 end
+
+def title(title = nil)
+    content_for :title, title
+end
