e769dbb2a79a4c9c2d4ee05cf8efe483234ec4aa
Ellymac <cschnell67@gmail.com>
2017-05-20 20:45:50 +0200

    news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,7 +5,7 @@ def show
   require 'open-uri'
   @rss_results = []
   rss = RSS::Parser.parse(open('http://www.dna.fr/rss').read, false).items[0..5]
-
+# http://www.ouest-france.fr/rss-en-continu.xml?region=bretagne
   # puts rss.map(&:inspect).join(', \n')
   rss.each do |result|
     result = { title: result.title, pubDate: result.pubDate, link: result.link, description: result.description }
