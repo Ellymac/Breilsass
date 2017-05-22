acfdf4dff1428f07e667c7246e31e06bc68748c2
prenan <renan.pincemin@outlook.fr>
2017-05-22 10:49:57 +0200

    up places Bretagne

--- app/assets/javascripts/placesBretagne.js
+++ app/assets/javascripts/placesBretagne.js
@@ -1,23 +1,63 @@
-function initMap() {
-  var myCenter = new google.maps.LatLng(48.246306,-4.489568);
-  var mapCanvas = document.getElementById("mapCrozon");
-  var mapOptions = {center: myCenter, zoom: 9};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({position:myCenter});
-
-  var myCenter2 = new google.maps.LatLng(47.704194,-2.953554);
-  var mapCanvas2 = document.getElementById("mapAnne");
-  var mapOptions2 = {center: myCenter2, zoom: 9};
-  var map2 = new google.maps.Map(mapCanvas2, mapOptions2);
-  var marker2 = new google.maps.Marker({position:myCenter2});
-
-  marker.setMap(map);
-  var infowindow = new google.maps.InfoWindow({
-    content: "Le bout du monde"
-  });
-  infowindow.open(map,marker);
-}
-
 $('.carousel').carousel({
   interval: 5000 //changes the speed
 })
+
+function initMap() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map.setTilt(45);
+
+    // Multiple Markers
+    var markers = [
+        ['Le bout du monde, Finistère', 48.246306,-4.489568],
+        ['Un peu de religion, Morbihan', 47.704194,-2.953554]
+    ];
+
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Le bout du monde, Finistère</h3>' +
+        "<a href='#crozon'>Plus d'informations ici</a>" +        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Un peu de religion, Morbihan</h3>' +
+        "<a href='#anne'>Plus d'informations ici</a>" +        '</div>']
+    ];
+
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    // Loop through our array of markers & place each one on the map
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+
+        // Allow each marker to have an info window
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(8);
+        google.maps.event.removeListener(boundsListener);
+    });
+
+}
